94756bed5eeaa7cf08791809ab72c92101e69b84
gness1804 <graham.nessler@gmail.com>
2016-09-07 15:04:19 -0600

    created rudiments of object to hold user idea info

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ $(document).ready(function () {
     function transferIdeaToBottom() {
       var $titleInput = $("#title-input").val();
       var $bodyInput = $("#body-input").val();
-    //   var inputObject = {};
+      var inputObject = {};
+      inputObject.title = "Dogs";
+      inputObject.body = "cats";
+      console.log(inputObject);
       createIdeaContainer($titleInput, $bodyInput); // needs to be corrected with storedInput variables
     }
 
