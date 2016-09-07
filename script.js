4b948845b3a6a5ad4296fdfa9e2dccc6c85028bb
gness1804 <graham.nessler@gmail.com>
2016-09-07 15:00:50 -0600

    slight refactoring of var names

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ $(document).ready(function () {
 
 
     function transferIdeaToBottom() {
-      var titleInput = $("#title-input").val();
-      var bodyInput = $("#body-input").val();
-      createIdeaContainer(titleInput, bodyInput);
+      var $titleInput = $("#title-input").val();
+      var $bodyInput = $("#body-input").val();
+    //   var inputObject = {};
+      createIdeaContainer($titleInput, $bodyInput); // needs to be corrected with storedInput variables
     }
 
-    function createIdeaContainer(titleInput, bodyInput) {
-        var newSection = "<section><h3>"+titleInput+"</h3><p>"+bodyInput+"</p><img src=''><img src=''><h6>Quality</h6></section>";
+    function createIdeaContainer($titleInput, $bodyInput) {
+        var newSection = "<section><h3>"+$titleInput+"</h3><p>"+$bodyInput+"</p><img src=''><img src=''><h6>Quality</h6></section>";
         $("#user-ideas").prepend(newSection);
     }
 
