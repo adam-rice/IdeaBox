8785c2a80d628956746721669f68169eace2528a
gness1804 <graham.nessler@gmail.com>
2016-09-07 14:24:27 -0600

    beginning basic input functions

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-var 
+$(document).ready(function () {
+
+    $("#save-button").click(function () {
+      transferIdeaToBottom();
+    });
+
+
+    function transferIdeaToBottom() {
+      var titleInput = $("#title-input").val();
+      var bodyInput = $("#body-input").val();
+      alert(titleInput);
+    }
+
+
+
+
+}); //end of jQuery body
