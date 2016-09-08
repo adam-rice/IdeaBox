c1533b107e05a9185e9a510adcf0ff5d356e305f
Adam Rice <adamcoltonrice@gmail.com>
2016-09-08 14:32:58 -0600

    styling for votes and delete buttons with svg change

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
     }
 
     function createIdeaContainer(newTitle, newBody, newQuality) {
-        var newSection = "<section id='each-idea-container'><h3>"+newTitle+"</h3><p>"+newBody+"</p><img id='upvote' src='images/upvote.svg'><img id='downvote' src='images/downvote.svg'><img id='delete' src='images/delete.svg'><h6><span id='designation-quality'>quality</span>: "+newQuality+"</h6><hr></section>";
+        var newSection = "<section id='each-idea-container'><h3>"+newTitle+"</h3><p>"+newBody+"</p><figure id='upvote'></figure><figure id='downvote'></figure><figure id='delete'></figure><h6><span id='designation-quality'>quality</span>: "+newQuality+"</h6><hr></section>";
         $("#user-ideas").prepend(newSection);
     }
 
