05fa52563af009988f8720f60d6c35f38b893af1
gness1804 <graham.nessler@gmail.com>
2016-09-08 16:12:33 -0600

    maybe some project

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var localArr = [];
+
     $("#save-button").click(function () {
       var $titleInput = $("#title-input").val();
       var $bodyInput = $("#body-input").val();
@@ -7,23 +9,7 @@ $(document).ready(function () {
       fillLocalArray($titleInput, $bodyInput);
     });
 
-  //   $("#title-input, #body-input").on("keyup keydown", function(key) { //the function to activate idea when user presses enter
-  //     debugger;
-  //     var $titleInput = $("#title-input").val();
-  //     var $bodyInput = $("#body-input").val();
-  //     if (key.which === 13) { // the enter key
-  //       transferIdeaToBottom($titleInput, $bodyInput);
-  //     }
-  // });
-
-  var localArr = [];
-
-    function transferIdeaToBottom($titleInput, $bodyInput) {
-
-      fillLocalArray();
-    }
-
-    function fillLocalArray() {
+    function fillLocalArray($titleInput, $bodyInput) {
       var inputObject = {};
       inputObject.title = $titleInput;
       inputObject.body = $bodyInput;
@@ -35,20 +21,37 @@ $(document).ready(function () {
 
 
 
+  //   $("#title-input, #body-input").on("keyup keydown", function(key) { //the function to activate idea when user presses enter
+  //     debugger;
+  //     var $titleInput = $("#title-input").val();
+  //     var $bodyInput = $("#body-input").val();
+  //     if (key.which === 13) { // the enter key
+  //       transferIdeaToBottom($titleInput, $bodyInput);
+  //     }
+  // });
 
-      var stringifiedArray = JSON.stringify(arr);
-      localStorage.setItem('array', stringifiedArray);
-      var stringifiedObj = JSON.stringify(inputObject);
-      localStorage.setItem('userInput', stringifiedObj);
-      var gottenItem = localStorage.getItem('userInput');
-      var parsed = JSON.parse(gottenItem);
-      var newTitle = parsed.title;
-      var newBody = parsed.body;
-      var newId = parsed.id;
-      var newQuality = parsed.quality;
-      createIdeaContainer(newTitle, newBody, newQuality);
-      clearInputFields();
-    }
+
+
+    // function transferIdeaToBottom($titleInput, $bodyInput) {
+
+
+
+
+
+
+    //   var stringifiedArray = JSON.stringify(arr);
+    //   localStorage.setItem('array', stringifiedArray);
+    //   var stringifiedObj = JSON.stringify(inputObject);
+    //   localStorage.setItem('userInput', stringifiedObj);
+    //   var gottenItem = localStorage.getItem('userInput');
+    //   var parsed = JSON.parse(gottenItem);
+    //   var newTitle = parsed.title;
+    //   var newBody = parsed.body;
+    //   var newId = parsed.id;
+    //   var newQuality = parsed.quality;
+    //   createIdeaContainer(newTitle, newBody, newQuality);
+    //   clearInputFields();
+    // }
 
     function createIdeaContainer(newTitle, newBody, newQuality) {
         var newSection = "<section id='each-idea-container'> <header><h3>"+newTitle+"</h3><figure id='delete'></figure></header> <p>"+newBody+"</p> <footer><figure id='upvote'></figure><figure id='downvote'></figure><h6><span id='designation-quality'>quality</span>: "+newQuality+"</h6></footer> <hr> </section>";
