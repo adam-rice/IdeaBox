4d8b7d6963cec05b89c926e047b45557935268d1
Adam Rice <adamcoltonrice@gmail.com>
2016-10-28 13:00:46 -0600

    file refactor

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ $(document).ready(function () {
 
   Idea.prototype.toHTML = function () {
     return $(`
-        <section class='each-idea-card' id=${this.id}>
-            <header>
-                <h3 contenteditable='true'>${this.title}</h3>
-                <figure class='delete' id='delete'></figure>
-            </header>
-                <p contenteditable='true'>${this.body}</p>
-            <footer>
-                <figure class='upvote' id='upvote'></figure>
-                <figure class='downvote' id='downvote'></figure>
-                <h6><span class='designation-quality'>quality</span>: ${this.quality}</h6>
-                <img src=${this.image} class='rating-img' />
-            </footer><hr>
-        </section>
+      <section class='each-idea-card' id=${this.id}>
+        <header>
+            <h3 contenteditable='true' class='content'>${this.title}</h3>
+            <figure class='delete' id='delete'></figure>
+        </header>
+        <p contenteditable='true' class='content'>${this.body}</p>
+        <footer>
+          <figure class='upvote' id='upvote'></figure>
+          <figure class='downvote' id='downvote'></figure>
+          <h6 class='content'><span class='designation-quality'>quality</span>: ${this.quality}</h6>
+          <img src=${this.image} class='rating-img' />
+        </footer><hr>
+      </section>
     `);
   };
 
@@ -37,13 +37,10 @@ $(document).ready(function () {
   };
 
   var ideaManager = {
-
     ideaArray: [],
-
     add: function (title, body) {
       this.ideaArray.push(new Idea(title, body));
       this.store();
-
     },
 
     find: function (id) {
@@ -51,16 +48,15 @@ $(document).ready(function () {
       return this.ideaArray.find(function (idea) {
         return idea.id === id;
       });
-    }, // end of find function
+    },
 
     render: function () {
       $userIdeas.html("");
       for (var i = 0; i < this.ideaArray.length; i++) {
         var idea = this.ideaArray[i];
-
         $userIdeas.prepend(idea.toHTML());
       }
-    }, // end of render function
+    },
 
     store: function () {
       localStorage.setItem("ideas", JSON.stringify(this.ideaArray));
@@ -75,7 +71,7 @@ $(document).ready(function () {
           this.ideaArray.push(new Idea(idea.title, idea.body, idea.id, idea.quality, idea.image));
         }
       }
-    }, // end of retrieve function
+    },
 
     remove: function (id) {
       id2 = parseInt(id);
@@ -84,12 +80,11 @@ $(document).ready(function () {
       });
       this.store();
     },
-
   }; // end of ideaManager
 
   function clearInputFields() {
-    $titleInput.val("");
-    $bodyInput.val("");
+    $titleInput.val('');
+    $bodyInput.val('');
   }
 
   function addUserInputToIdeaManager() {
@@ -113,40 +108,36 @@ $(document).ready(function () {
     var find = ideaManager.find(id);
     if (this.id === "upvote") {
       find.upvote();
-    }
-    else if (this.id === "downvote") {
+     } else if (this.id === "downvote") {
       find.downvote();
-    }
-    else if (this.id === "delete") {
+     } else if (this.id === "delete") {
       find.remove();
     }
-  }); // end of click, delete, and upvote function
+  });
 
   Idea.prototype.upvote = function () {
     var quality = this.quality;
     if (quality === "swill") {
       this.quality = "plausible";
       this.image = "images/thinking.png";
-    }
-    else if (quality === "plausible") {
+      } else if (quality === "plausible") {
       this.quality = "genius";
       this.image = "images/light-bulb.jpg";
     }
-      ideaManager.store();
-  }; //end of upvote
+    ideaManager.store();
+  };
 
   Idea.prototype.downvote = function () {
     var quality = this.quality;
     if (quality === "genius") {
       this.quality = "plausible";
       this.image = "images/thinking.png";
-    }
-    else if (quality === "plausible") {
+      } else if (quality === "plausible") {
       this.quality = "swill";
       this.image = "images/pig.png";
     }
     ideaManager.store();
-  }; //end of downvote
+  };
 
   $userIdeas.on("keyup keydown click", "h3, p", function (key) {
     var id = $(this).closest(".each-idea-card").attr("id");
@@ -155,13 +146,12 @@ $(document).ready(function () {
       if (event.target.nodeName === "H3") {
         var targetH3 = $(this).closest("h3").text();
         ideaManager.find(id).saveEditableTitle(targetH3);
-      }
-      else if (event.target.nodeName === "P") {
+        } else if (event.target.nodeName === "P") {
         var targetP = $(this).closest("p").text();
         ideaManager.find(id).saveEditableBody(targetP);
       }
-    } // end of big "enter" if statement
-  }); //end of keyup keydown click function
+    }
+  });
 
   $userIdeas.on("blur", "h3, p", function () {
     var id = $(this).closest(".each-idea-card").attr("id");
@@ -169,12 +159,11 @@ $(document).ready(function () {
     if (event.target.nodeName === "H3") {
       var targetH3 = $(this).closest("h3").text();
       ideaManager.find(id).saveEditableTitle(targetH3);
-    }
-    else if (event.target.nodeName === "P") {
+      } else if (event.target.nodeName === "P") {
       var targetP = $(this).closest("p").text();
       ideaManager.find(id).saveEditableBody(targetP);
     }
-  }); //end of blur function
+  });
 
   Idea.prototype.saveEditableTitle = function (target) {
     this.title = target;
@@ -187,17 +176,12 @@ $(document).ready(function () {
   };
 
   $searchInput.on("keyup", function () {
-    var search = $(this).val().trim();
+    var search = $(this).val();
     $("h3:contains('" + search + "')").closest(".each-idea-card").show();
     $("h3:not(:contains('" + search + "'))").closest(".each-idea-card").hide();
-  });
-
-  $searchInput.on("keyup", function () {
-    var search = $(this).val().trim();
     $("p:contains('" + search + "')").closest(".each-idea-card").show();
   });
 
   ideaManager.retrieve();
   ideaManager.render();
-
-}); //end of jQuery body
+});
